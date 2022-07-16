181703ba0546a40df99f911b9ca44c76fb14bcfa
pankun-coder <jjohese@gmail.com>
2022-07-16 16:44:50 +0900

    minor fixes

--- frontend/pages/addStudent.js
+++ frontend/pages/addStudent.js
@@ -5,20 +5,22 @@ import { useContext } from "react";
 import { isLoggedInContext } from "../lib/isLoggedInContext";
 import LoginRequiredModal from "../components/loginRequiredModal";
 import BorderM from "../components/borderM";
+import MessageModal from "../components/messageModal";
 
 export default function AddUser() {
-    const {isLoggedIn} = useContext(isLoggedInContext);
+    const {isLoggedIn, setIsLoggedIn} = useContext(isLoggedInContext);
     const [studentName, setStudentName] = useState("");
+    const [message, setMessage] = useState({body: "", isError: false});
 
-    const createUserHandle = () => {
-        const url = `http://${process.env.NEXT_PUBLIC_BACKEND_HOST}:3001/api/v0/students`
-        const data = {name: studentName}
+    const createUser = () => {
+        const url = `${process.env.NEXT_PUBLIC_BACKEND_DOMAIN}/api/v0/students`;
+        const data = {name: studentName};
         axios.post(url, data, {withCredentials: true})
-        .then(res => {
-            console.log(res)
+        .then(response => {
+            setMessage({body: response.data.message, isError: false});
         })
         .catch(error => {
-            console.log(error)
+            setMessage({body: error.response.data.message, isError: true});
         })
     }
 
@@ -38,11 +40,10 @@ export default function AddUser() {
                         <label htmlFor="student-name">生徒名:</label>
                         <input className="border-2 border-black w-42" id="student-name" onChange={(e) => {setStudentName(e.target.value)}} value={studentName} />
                     </div>
-                    <button className="border-2 border-black hover:bg-purple-400 hover:text-white hover:border-purple-400" onClick={() => {createUserHandle()}}>生徒登録</button>
+                    <button type="button" className="border-2 border-black hover:bg-purple-400 hover:text-white hover:border-purple-400" onClick={() => {createUser()}}>生徒登録</button>
                 </form>
             </BorderM>
+            {message.body&&<MessageModal message={message.body} isError={message.isError} onClickClose={() => {setMessage({body: "", isError: false})}}></MessageModal>}            
         </Layout>
-
-
     )
 }
