909644a9de279fad04c377552dca8271d8fbe79c
pankun-coder <jjohese@gmail.com>
2022-11-14 00:21:32 +0900

    add type annotation to some components

--- components/common/pageTitle.js
+++
@@ -1,3 +0,0 @@
-export default function PageTitle(props) {
-  return <h1 className="mt-4 mb-8 text-2xl">{props.value}</h1>;
-}
---
+++ components/common/pageTitle.tsx
@@ -0,0 +1,3 @@
+export default function PageTitle({ value }: { value: string }) {
+  return <h1 className="mt-4 mb-8 text-2xl">{value}</h1>;
+}
